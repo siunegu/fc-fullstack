c858e6e073983762310b1c06b5a650bb64cdd27f
JAMES LEBOEUF <jamesleboeuf@JAMESs-MBP.home.gateway>
2015-08-08 14:01:21 +1000

    techspec-footer-directive: almost finished the contact page

--- client/src/app/contact/contact.controller.js
+++ client/src/app/contact/contact.controller.js
@@ -1,8 +1,32 @@
 class ContactController {
-  constructor () {
+  constructor ($http, ngDialog) {
     'ngInject';
 
     console.log("initializing ContactController");
+  }
+
+  sendEmail(form) {
+
+    var data = {
+      contactName: form.name,
+      contactEmail: form.email
+      // TODO: Add country as data to send
+    };
+
+    this.$http.post('http://localhost:9030/email', data)
+    .then(function(result){
+      console.log('success is...', result);
+      return result;
+    })
+    .catch(function(error){
+      console.log('error is...', error);
+      return error;
+    })
+
+    console.log(form);
+    this.ngDialog.open({
+      template: '/app/preorder/popupTmpl.html'
+    });
   }
 }
 

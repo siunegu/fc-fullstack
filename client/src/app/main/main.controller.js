03b8b67f96de6378d6f1337743d18cc6f97bf4a0
JAMES LEBOEUF <jamesleboeuf@JAMESs-MBP.home.gateway>
2015-08-22 14:59:05 +1000

    fixed next and previous buttons on the home slider

--- client/src/app/main/main.controller.js
+++ client/src/app/main/main.controller.js
@@ -1,30 +1,35 @@
-class MainController {
+  class MainController {
 
   constructor($timeout, ngDialog, $scope, $document) {
     'ngInject';
     console.log('initialising MainController');
 
-    angular.element(document).ready(function() {
-        $scope.ngDialog = ngDialog;
+    angular.element(document).ready(function(){
+      $scope.ngDialog = ngDialog;
 
-        $scope.slickConfig = {
-          autoplay: true,
-          draggable: false,
-          autoplaySpeed: 3000,
-          method: {},
-          event: {
-            beforeChange: function(event, slick, currentSlide, nextSlide) {},
-            afterChange: function(event, slick, currentSlide, nextSlide) {}
-          };
-        });
-    }
+      $scope.slickConfig = {
+        autoplay: true,
+        draggable: false,
+        autoplaySpeed: 3000,
+        method: {},
+        event: {
+          beforeChange: function (event, slick, currentSlide, nextSlide) {
+          },
+          afterChange: function (event, slick, currentSlide, nextSlide) {
+          }
+        }
+      };
+    });
 
-    openModal(htmlFile) {
-      this.ngDialog.open({
-        template: './app/dialogs/' + htmlFile + '.html'
-      });
-    }
 
   }
 
-  export default MainController;
+  openModal(htmlFile){
+    this.ngDialog.open({
+      template: './app/dialogs/' + htmlFile + '.html'
+    });
+  }
+
+}
+
+export default MainController;

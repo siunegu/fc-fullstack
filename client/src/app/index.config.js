1702684c514f02f2e2fcd4e19ed9e8c7d6719caf
JAMES LEBOEUF <jamesleboeuf@JAMESs-MacBook-Pro.local>
2015-09-03 13:16:57 +1000

    removed hashtags from url's

--- client/src/app/index.config.js
+++ client/src/app/index.config.js
@@ -1,12 +1,15 @@
-function config ($logProvider, $httpProvider) {
+function config ($logProvider, $httpProvider, $locationProvider) {
 	'ngInject';
   // Enable log
   $logProvider.debugEnabled(true);
 
+	// Disable Hash tags from URL
+	$locationProvider.html5Mode(true);
+
   // CORS
   $httpProvider.defaults.useXDomain = true;
   delete $httpProvider.defaults.headers.common['X-Requested-With'];
-  
+
 
 }
 
